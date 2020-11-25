b9610bb67b751d79f8792c074edbb3cbd4074806
junilHwang <junil.h@kakao.com>
2020-11-25 23:45:58 +0900

    test: afterEach로 초기화k

--- test/counter.spec.js
+++ test/counter.spec.js
@@ -3,9 +3,14 @@ import { createCounter } from '../src/counter';
 // Step 1
 describe('옵션이 지정되지 않은 경우', () => {
   let counter;
+
   beforeEach(() => {
     counter = createCounter();
   })
+
+  afterEach(() => {
+    counter = undefined;
+  });
 
   it('초기값은 0이다.', () => {
     expect(counter.val()).toBe(0);
@@ -40,10 +45,17 @@ it('initValue 옵션 사용 시 초기값이 해당 값으로 지정된다.', ()
 describe('min 옵션 사용 시 현재값과 min 값이 동일하면', () => {
   let counter;
   let min;
+
   beforeEach(() => {
     min = 0;
     counter = createCounter({ min })
-  })
+  });
+
+  afterEach(() => {
+    min = undefined;
+    counter = undefined;
+  });
+
   it('dec() 함수를 호출해도 값이 감소하지 않는다.', () => {
     counter.dec();
     expect(counter.val()).toBe(min);
@@ -57,10 +69,17 @@ describe('min 옵션 사용 시 현재값과 min 값이 동일하면', () => {
 describe('max 옵션 사용 시 현재값과 max 값이 동일하면', () => {
   let counter;
   let max;
+
   beforeEach(() => {
     max = 0;
     counter = createCounter({ max })
-  })
+  });
+
+  afterEach(() => {
+    max = undefined;
+    counter = undefined;
+  });
+
   it('inc() 함수를 호출해도 값이 증가하지 않는다.', () => {
     counter.inc();
     expect(counter.val()).toBe(max);
