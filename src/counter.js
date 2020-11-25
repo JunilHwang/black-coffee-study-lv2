3c09bf32f78ab94b138a79dd24925638f8daa284
junilHwang <junil.h@kakao.com>
2020-11-25 23:42:07 +0900

    test: counter test 작성 완료

--- src/counter.js
+++ src/counter.js
@@ -1,13 +1,24 @@
-export function createCounter() {
+export function createCounter({
+  initVal = 0,
+  min = -Infinity,
+  max = Infinity
+} = {}) {
+  let count = initVal;
   return {
     val() {
-
+      return count;
     },
     inc() {
-
+      count = Math.min(count + 1, max);
     },
     dec() {
-
+      count = Math.max(count - 1, min);
+    },
+    isMax() {
+      return count === max;
+    },
+    isMin() {
+      return count === min;
     }
   };
 }
--- test/counter.spec.js
+++ test/counter.spec.js
@@ -2,46 +2,71 @@ import { createCounter } from '../src/counter';
 
 // Step 1
 describe('옵션이 지정되지 않은 경우', () => {
-  it('초기값은 0이다.', () => {
+  let counter;
+  beforeEach(() => {
+    counter = createCounter();
+  })
 
+  it('초기값은 0이다.', () => {
+    expect(counter.val()).toBe(0);
   });
 
   it('inc() 함수는 값을 1증가시킨다.', () => {
-
+    counter.inc();
+    expect(counter.val()).toBe(1);
   });
 
   it('dec() 함수는 값을 1감소시킨다.', () => {
-
+    counter.dec();
+    expect(counter.val()).toBe(-1);
   });
 
   it('isMax() 호출시 false를 반환한다.', () => {
-
+    expect(counter.isMax()).toBe(false);
   });
 
   it('isMin() 호출시 false를 반환한다.', () => {
-
+    expect(counter.isMin()).toBe(false);
   });
 });
 
 // Step 2
-it('initValue 옵션 사용 시 초기값이 해당 값으로 지정된다.', () => {});
+it('initValue 옵션 사용 시 초기값이 해당 값으로 지정된다.', () => {
+  const initVal = 10;
+  const counter = createCounter({ initVal });
+  expect(counter.val()).toBe(initVal);
+});
 
 describe('min 옵션 사용 시 현재값과 min 값이 동일하면', () => {
+  let counter;
+  let min;
+  beforeEach(() => {
+    min = 0;
+    counter = createCounter({ min })
+  })
   it('dec() 함수를 호출해도 값이 감소하지 않는다.', () => {
-
+    counter.dec();
+    expect(counter.val()).toBe(min);
   });
 
   it('isMin() 호출 시 true를 반환한다.', () => {
-
+    expect(counter.isMin()).toBe(true);
   });
 });
 
 describe('max 옵션 사용 시 현재값과 max 값이 동일하면', () => {
+  let counter;
+  let max;
+  beforeEach(() => {
+    max = 0;
+    counter = createCounter({ max })
+  })
   it('inc() 함수를 호출해도 값이 증가하지 않는다.', () => {
-
+    counter.inc();
+    expect(counter.val()).toBe(max);
   });
 
   it('isMax() 호출 시 true를 반환한다.', () => {
-
+    expect(counter.isMax()).toBe(true);
   });
 });
