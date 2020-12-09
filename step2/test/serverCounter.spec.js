84136e9fa5625f6d790354d6c45168f1f8695cc7
JunilHwang <junil.h@kakao.com>
2020-12-09 15:59:18 +0900

    feat: serverCounter 작업 완료

--- step2/test/serverCounter.spec.js
+++ step2/test/serverCounter.spec.js
@@ -4,6 +4,7 @@ import MockAdapter from 'axios-mock-adapter';
 import { waitForDomChange, wait, getByText, fireEvent } from '@testing-library/dom';
 import {createCounter} from "../src/backup/counter";
 import {createServerCounter} from "../src/serverCounter/counter";
+import {waitFor} from "@babel/core/lib/gensync-utils/async";
 
 const mockAxios = new MockAdapter(axios);
 
@@ -15,13 +16,13 @@ const counter = createCounter({
 
 let container;
 
-beforeEach(() => {
+const response = () => ({
+  value: counter.val(),
+  isMax: counter.isMax(),
+  isMin: counter.isMin()
+});
 
-  const response = () => ({
-    value: counter.val(),
-    isMax: counter.isMax(),
-    isMin: counter.isMin()
-  });
+beforeEach(() => {
 
   mockAxios.onGet('/counter').reply(() => [200, response()]);
   mockAxios.onPut('/counter/inc').reply(() => {
@@ -50,16 +51,35 @@ it('생성시 버튼과 초기값을 렌더링한다.', async () => {
 });
 
 it('+ 버튼 클릭시 서버에 inc요청을 보낸 후 응답값으로 뷰를 갱신한다.', async () => {
+  await waitFor(() => getByText(container, '+').click());
+  await waitFor(() => expect(getByText(container, "11")).toBeTruthy());
 });
 
 it('- 버튼 클릭시 서버에 dec 요청을 보낸 후 응답값으로 뷰를 갱신한다.', async () => {
-
+  await waitFor(() => getByText(container, '-').click());
+  await waitFor(() => expect(getByText(container, "9")).toBeTruthy());
 });
 
 it('최대값이면 + 버튼이 disabled 상태가 되고, 클릭해도 서버에 요청을 보내지 않는다', async () => {
-
+  await waitFor(() => getByText(container, '+').click());
+  await waitFor(() => getByText(container, '+').click());
+  await waitFor(() => expect(getByText(container, "+", {selector: "[disabled]"})).toBeTruthy());
+  await waitFor(() => getByText(container, '+').click());
+  await waitFor(() => getByText(container, '+').click());
+  await waitFor(() => getByText(container, '+').click());
+  await waitFor(() => getByText(container, '+').click());
+  await waitFor(() => getByText(container, '+').click());
+  await waitFor(() => expect(getByText(container, "12")).toBeTruthy());
 });
 
 it('최소값이면 - 버튼이 disabled 상태가 되고, 클릭해도 서버에 요청을 보내지 않는다', async () => {
-
+  await waitFor(() => getByText(container, '-').click());
+  await waitFor(() => getByText(container, '-').click());
+  await waitFor(() => expect(getByText(container, '-', {selector: "[disabled]"})).toBeTruthy());
+  await waitFor(() => getByText(container, '-').click());
+  await waitFor(() => getByText(container, '-').click());
+  await waitFor(() => getByText(container, '-').click());
+  await waitFor(() => getByText(container, '-').click());
+  await waitFor(() => getByText(container, '-').click());
+  await waitFor(() => expect(getByText(container, "8")).toBeTruthy());
 });
