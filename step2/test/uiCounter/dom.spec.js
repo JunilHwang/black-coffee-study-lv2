a2112183f7e4f94d804d9aeac2def851be795adb
JunilHwang <junil.h@kakao.com>
2020-12-09 14:45:14 +0900

    feat: uiCounter test 작업 완료

--- step2/test/uiCounter/dom.spec.js
+++ step2/test/uiCounter/dom.spec.js
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import {getByText, fireEvent, wait} from '@testing-library/dom';
-import { createUICounter } from '../../src/uiCounter/counter';
+import {createUICounter} from '../../src/uiCounter/counter';
 
 let container;
 
@@ -26,31 +26,43 @@ it('생성시 버튼과 초기값을 렌더링한다.', () => {
 });
 
 it('+ 버튼 클릭시 1 증가한다.', async () => {
-  fireEvent(
-    getByText(container, '+'),
-    new MouseEvent('click', {
-      bubbles: true,
-      cancelable: true,
-    })
-  );
+  fireEvent.click(getByText(container, '+'));
   await wait(() => expect(getByText(container, "11")).toBeTruthy());
 });
 
 it('- 버튼 클릭시 1 감소한다.', async () => {
-  fireEvent(
-    getByText(container, '-'),
-    new MouseEvent('click', {
-      bubbles: true,
-      cancelable: true,
-    })
-  );
+  fireEvent.click(getByText(container, '-'));
   await wait(() => expect(getByText(container, "9")).toBeTruthy());
 });
 
-it('Max값인 경우 + 버튼이 disabled 상태가 되며 클릭해도 증가하지 않는다.', () => {
-
+it('Max값인 경우 + 버튼이 disabled 상태가 되며 클릭해도 증가하지 않는다.', async () => {
+  fireEvent.click(getByText(container, '+'));
+  fireEvent.click(getByText(container, '+'));
+  await wait(() =>
+    expect(getByText(container, "+", {selector: "[disabled]"})).toBeTruthy()
+  );
+  fireEvent.click(getByText(container, '+'));
+  fireEvent.click(getByText(container, '+'));
+  fireEvent.click(getByText(container, '+'));
+  fireEvent.click(getByText(container, '+'));
+  fireEvent.click(getByText(container, '+'));
+  await wait(
+    () => expect(getByText(container, "12")).toBeTruthy()
+  );
 });
 
-it('Min값인 경우 - 버튼이 disabled 상태가 되며, 클릭해도 감소하지 않는다.', () => {
-
+it('Min값인 경우 - 버튼이 disabled 상태가 되며, 클릭해도 감소하지 않는다.', async () => {
+  fireEvent.click(getByText(container, '-'));
+  fireEvent.click(getByText(container, '-'));
+  await wait(() =>
+    expect(getByText(container, '-', {selector: "[disabled]"})).toBeTruthy()
+  );
+  fireEvent.click(getByText(container, '-'));
+  fireEvent.click(getByText(container, '-'));
+  fireEvent.click(getByText(container, '-'));
+  fireEvent.click(getByText(container, '-'));
+  fireEvent.click(getByText(container, '-'));
+  await wait(
+    () => expect(getByText(container, "8")).toBeTruthy()
+  );
 });
