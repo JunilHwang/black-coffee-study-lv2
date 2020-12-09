ddee6615a25e36ab5af881ba319f184774e00d3c
JunilHwang <junil.h@kakao.com>
2020-12-09 23:20:10 +0900

    feat: TodoList를 읽어오는 테스트 완료

--- step2/test/todoapp/mockApi.js
+++ step2/test/todoapp/mockApi.js
@@ -6,9 +6,15 @@ import {todoBaseURL} from "../../src/todoapp/adapter/todoAdapter";
 const mockAxios = new MockAdapter(axios);
 
 export function todoMockInit () {
-  mockAxios.onGet(todoBaseURL + "/users").reply(() => {
+  mockAxios.onGet(`${todoBaseURL}/users`).reply(() => {
     console.log(todoList);
     return [200, todoList];
+  });
+  const userId = "1607513733976";
+  mockAxios.onGet(`${todoBaseURL}/users/${userId}/items`).reply(() => {
+    const items = todoList.find(v => v._id === userId).todoList;
+    console.log(items);
+    return [200, items];
   });
 }
 
--- step2/test/todoapp/todoapp.spec.js
+++ step2/test/todoapp/todoapp.spec.js
@@ -1,6 +1,6 @@
 import { todoMockInit, todoMockReset } from './mockApi';
 import App from "../../src/todoapp/App";
-import {getByText, waitForDomChange} from "@testing-library/dom";
+import {getAllByText, getByText, queryAllByText, waitForDomChange} from "@testing-library/dom";
 import {waitFor} from "@babel/core/lib/gensync-utils/async";
 import axios from "axios";
 import {todoBaseURL} from "../../src/todoapp/adapter/todoAdapter";
@@ -21,7 +21,19 @@ afterEach(() => {
 });
 
 it('투두리스트를 읽어오는 통신 테스트', async () => {
+  // 유저에 대한 검증
   await waitForDomChange({ container });
   const $name = getByText(container, "junil", { selector: "button[data-ref='select']" });
+  await waitFor(() => expect($name).toBeVisible());
+
+  // 타이블 변경 확인
   $name.click();
+  await waitForDomChange({ container });
+  const $title = getByText(container, "junil", { selector: "strong" });
+  expect($title.parentElement.outerHTML).toEqual("<span><strong>junil</strong>'s Todo List</span>");
+
+  // 아이템 리스트 확인
+  await waitForDomChange({ container });
+  const $items = getAllByText(container, /111|222|333/, { selector: "label[data-ref='contents']" });
+  expect($items.length).toEqual(3);
 });
