92c40fd09b08c73f32013532b1c658c6aae9a10b
JunilHwang <junil.h@kakao.com>
2020-12-10 00:39:11 +0900

    test: TodoList 필터링 테스트 완료

--- step2/test/todoapp/mockApi.js
+++ step2/test/todoapp/mockApi.js
@@ -8,19 +8,19 @@ const mockAxios = new MockAdapter(axios);
 export function todoMockInit () {
 
   const userId = "1607513733976";
-  const itemId = "202012092343";
-  const deleteItemId = "1607513733979";
+  const itemId1 = "202012092343";
+  const itemId2 = "1607513733979";
 
   mockAxios
     .onGet(`${todoBaseURL}/users`)
       .reply(() => {
-        console.log(todoList);
+        // console.log(todoList);
         return [200, todoList];
       })
     .onGet(`${todoBaseURL}/users/${userId}/items`)
       .reply(() => {
         const items = todoList.find(v => v._id === userId).todoList;
-        console.log(items);
+        // console.log(items);
         return [200, items];
       })
     .onPost(`${todoBaseURL}/users/${userId}/items`)
@@ -28,28 +28,28 @@ export function todoMockInit () {
         const { contents } = JSON.parse(data);
         const items = todoList.find(v => v._id === userId).todoList;
         const newItem = {
-          _id: itemId,
+          _id: itemId1,
           isCompleted: false,
           priority: "NONE",
           contents,
         };
         items.push(newItem);
-        console.log(newItem);
+        // console.log(newItem);
         return [200, newItem];
       })
-    .onPut(`${todoBaseURL}/users/${userId}/items/${itemId}`)
+    .onPut(`${todoBaseURL}/users/${userId}/items/${itemId2}`)
       .reply(({ data }) => {
         const { contents } = JSON.parse(data);
         const items = todoList.find(v => v._id === userId).todoList;
-        const item = items.find(v => v._id === itemId);
+        const item = items.find(v => v._id === itemId2);
         item.contents = contents;
-        console.log(item);
+        // console.log(item);
         return [200, item];
       })
-    .onDelete(`${todoBaseURL}/users/${userId}/items/${deleteItemId}`)
+    .onDelete(`${todoBaseURL}/users/${userId}/items/${itemId2}`)
       .reply(() => {
         const items = todoList.find(v => v._id === userId).todoList;
-        const itemIndex = items.findIndex(v => v._id === deleteItemId);
+        const itemIndex = items.findIndex(v => v._id === itemId2);
         items.splice(itemIndex, 1);
         return [200];
       })
--- step2/test/todoapp/todoapp.spec.js
+++ step2/test/todoapp/todoapp.spec.js
@@ -60,8 +60,10 @@ it('투두리스트를 생성하는 통신 테스트', async () => {
   await waitForDomChange({ container }); // 로딩 완료
 
   // 아이템 컨텐츠 및 갯수 검증
-  const $items = getAllByText(container, /111|222|333|444/, { selector: "label[data-ref='contents']" });
-  expect($items.length).toEqual(4);
+  await waitFor(() => {
+    const $items = getAllByText(container, /111|222|333|444/, { selector: "label[data-ref='contents']" });
+    expect($items.length).toEqual(4);
+  })
 });
 
 it('투두리스트를 수정하는 통신 테스트', async () => {
@@ -71,32 +73,14 @@ it('투두리스트를 수정하는 통신 테스트', async () => {
   await waitForDomChange({ container }); // 로딩
   await waitForDomChange({ container }); // 로딩 완료
 
-  // 아이템 추가
-  const $appender = getByPlaceholderText(container, "할 일을 입력해주세요.");
-  $appender.value = "444";
-  fireEvent.keyPress($appender, { key: 'Enter' });
+  // 아이템 수정
+  fireEvent.dblClick(getByText(container, "333", { selector: "label[data-ref='contents']" }));
+  await waitForDomChange({ container });
+
+  fireEvent.keyPress(getByDisplayValue(container, "333"), { key: 'Enter', target: { value: "333 change" } });
   await waitForDomChange({ container }); // 로딩 완료
 
-  // 아이템 컨텐츠 및 갯수 검증
-  const $items = getAllByText(container, /111|222|333|444/, { selector: "label[data-ref='contents']" });
-  expect($items.length).toEqual(4);
-});
-
-it('투두리스트를 수정하는 통신 테스트', async () => {
-  // 유저 선택
-  await waitForDomChange({ container });
-  getByText(container, "junil", { selector: "button[data-ref='select']" }).click();
-  await waitForDomChange({ container }); // 로딩
-  await waitForDomChange({ container }); // 로딩 완료
-
-  // 아이템 수정 인풋 보이기
-  fireEvent.dblClick(getByText(container, "444"));
-  await waitForDomChange({ container });
-
-  fireEvent.keyPress(getByDisplayValue(container, "444"), { key: "Enter", target: { value: "444 change" } });
-  await waitForDomChange({ container });
-
-  await waitFor(() => expect(getByText(container, "444 change")).toBeVisible());
+  await waitFor(() => expect(getByText(container, "333 change")).toBeVisible());
 });
 
 it('투두리스트를 삭제하는 통신 테스트', async () => {
@@ -107,11 +91,93 @@ it('투두리스트를 삭제하는 통신 테스트', async () => {
   await waitForDomChange({ container }); // 로딩 완료
 
   // 아이템 선택 후 삭제
-  const $itemLabel = getByText(container, "333", { selector: "label" });
-  fireEvent.click($itemLabel.nextElementSibling);
+  fireEvent.click(getByText(container, /333/, { selector: "label" }).nextElementSibling);
   await waitForDomChange({ container });
 
   // 아이템 컨텐츠 및 갯수 검증
-  const $items = getAllByText(container, /111|222|333|444/, { selector: "label[data-ref='contents']" });
+  const $items = getAllByText(container, /111|222|333/, { selector: "label[data-ref='contents']" });
   expect($items.length).toEqual(2);
+});
+
+it('투두리스트의 아이템 갯수 카운트 테스트', async () => {
+  // 유저 선택
+  await waitForDomChange({ container });
+  getByText(container, "junil", { selector: "button[data-ref='select']" }).click();
+  await waitForDomChange({ container }); // 로딩
+  await waitForDomChange({ container }); // 로딩 완료
+
+  // 아이템 컨텐츠 및 갯수 검증
+  await waitFor(() => {
+    const $items = getAllByText(container, /.*/, { selector: "label[data-ref='contents']" });
+    expect($items.length).toEqual(3);
+  })
+});
+
+it('todo list의 상태값 확인 테스트', async () => {
+  // 유저 선택
+  await waitForDomChange({ container });
+  getByText(container, "junil", { selector: "button[data-ref='select']" }).click();
+  await waitForDomChange({ container }); // 로딩
+  await waitForDomChange({ container }); // 로딩 완료
+
+  // 상태값 확인 테스트
+
+  await waitFor(() => {
+    const $item1 = getByText(container, "111", { selector: "label[data-ref='contents']" });
+    expect($item1.previousElementSibling.checked).toEqual(true)
+  });
+
+  await waitFor(() => {
+    const $item2 = getByText(container, "222", { selector: "label[data-ref='contents']" });
+    expect($item2.previousElementSibling.checked).toEqual(false)
+  });
+
+  await waitFor(() => {
+    const $item3 = getByText(container, "333", { selector: "label[data-ref='contents']" });
+    expect($item3.previousElementSibling.checked).toEqual(false)
+  });
+});
+
+it('해야할 일 필터링 테스트', async () => {
+  // 유저 선택
+  await waitForDomChange({ container });
+  getByText(container, "junil", { selector: "button[data-ref='select']" }).click();
+  await waitForDomChange({ container }); // 로딩
+  await waitForDomChange({ container }); // 로딩 완료
+
+  // 해야할 일 필터링
+  fireEvent.click(getByText(container, "해야할 일"));
+  await waitForDomChange({ container });
+
+  // 필터링 테스트
+  await waitFor(() => {
+    const $items1 = getAllByText(container, /111|222|333/, { selector: "label[data-ref='contents']" });
+    const $items2 = getAllByText(container, /222|333/, { selector: "label[data-ref='contents']" });
+    expect($items1.length).toEqual($items2.length);
+    expect($items1.length).toEqual(2);
+    expect($items2.length).toEqual(2);
+  });
+
+});
+
+it('완료한 일 필터링 테스트', async () => {
+  // 유저 선택
+  await waitForDomChange({ container });
+  getByText(container, "junil", { selector: "button[data-ref='select']" }).click();
+  await waitForDomChange({ container }); // 로딩
+  await waitForDomChange({ container }); // 로딩 완료
+
+  // 해야할 일 필터링
+  fireEvent.click(getByText(container, "완료한 일"));
+  await waitForDomChange({ container });
+
+  // 필터링 테스트
+  await waitFor(() => {
+    const $items1 = getAllByText(container, /111|222|333/, { selector: "label[data-ref='contents']" });
+    const $items2 = getAllByText(container, "111", { selector: "label[data-ref='contents']" });
+    expect($items1.length).toEqual($items2.length);
+    expect($items1.length).toEqual(1);
+    expect($items2.length).toEqual(1);
+  });
+
 });
