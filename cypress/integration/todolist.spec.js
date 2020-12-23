205d576f453fa0fbf8268e202d6b3e446311aebc
JunilHwang <junil.h@kakao.com>
2020-12-23 22:13:11 +0900

    fix: 테스트 오류 수정 중 (2)

--- cypress/integration/todolist.spec.js
+++ cypress/integration/todolist.spec.js
@@ -32,66 +32,59 @@ describe("TodoList 테스트", () => {
   });
 
   it("todo list에 todoItem을 키보드로 입력후 enter키를 눌러 추가하기", () => {
+    cy.get(".todoapp-container:eq(0)").as("container");
+    cy.get("@container").find(".new-todo").type(newTodoText).type('{enter}');
     cy.intercept("GET", `${baseURL}/teams/**/members/**`).as("getTodo");
-    cy.get(".todoapp-container").first().as("container");
-    cy.get("@container").get(".new-todo").type(newTodoText).type('{enter}');
     cy.wait("@getTodo");
-    cy.get("@container").get(".todo-list li").first().contains(newTodoText).should("be.visible");
+    cy.get("@container").find(".todo-list li:eq(0)").contains(newTodoText).should("be.visible");
   });
 
   it("todo list의 체크박스를 클릭하여 complete 상태로 변경.", () => {
     // li tag 에 completed class 추가, input 태그에 checked 속성 추가
-    cy.intercept("GET", `${baseURL}/teams/**/members/**`).as("getTodo");
     cy.get(".todoapp-container").first().get(".todo-list li").first().as("container");
     cy.get("@container").get(".toggle").first().click();
+    cy.intercept("GET", `${baseURL}/teams/**/members/**`).as("getTodo");
     cy.wait("@getTodo");
     cy.get("@container").should("have.class", "completed");
     cy.get("@container").get(".toggle").should("be.checked");
 
     cy.get("@container").get(".toggle").first().click();
     cy.wait("@getTodo");
-    cy.get("@container").should("not.have.class", "completed");
     cy.get("@container").get(".toggle").should("not.be.checked");
-  });
-
-  it("todoItem을 더블 클릭시 input 모드로 변경", () => {
-    cy.get(".todoapp-container").first().get(".todo-list li").first().as("container");
-    cy.get("@container").dblclick();
-    cy.get("@container").should("have.class", "editing");
-  });
-
-  it("todoItem의 수정 상태에서 esc 입력시 view 모드로 변경", () => {
-    cy.get(".todoapp-container").first().get(".todo-list li").first().as("container");
-    cy.get("@container").get("input").type(`{esc}`)
-    cy.get("@container").should("not.have.class", "editing");
-  });
-
-  it("todoItem을 수정", () => {
-    const updatedTodoText = `${newTodoText} update`;
-    cy.intercept("GET", `${baseURL}/teams/**/members/**`).as("getTodo");
-    cy.get(".todoapp-container").first().get(".todo-list li").first().as("container");
-    cy.get("@container").get("label").dblclick();
-    cy.get("@container").get("input").type(" update").type('{enter}');
-    cy.wait("@getTodo");
-    cy.get("@container").get('label').should("text", updatedTodoText);
   });
 
   it("todoItem의 갯수를 counting", () => {
     cy.get(".todoapp-container").first().as("container");
-    cy.get("@container").get(".todo-list").children().should("have.length", 1);
     cy.get("@container").get(".todo-count strong").should("text", "1");
   });
 
-  it("todoItem을 삭제하기", () => {
+  it("todoItem을 수정", () => {
+    cy.get(".todoapp-container:eq(0) .todo-list li:eq(0)").as("container");
+    cy.get("@container").find("label").trigger('dblclick');
+    cy.get("@container").find("input.edit").focus().type(" update ", { force: true }).type('{enter}', { force: true });
+
     cy.intercept("GET", `${baseURL}/teams/**/members/**`).as("getTodo");
-    cy.get(".todoapp-container").first().as("container");
-    cy.get("@container").first().get(".todo-list li").first().get(".destroy").click();
     cy.wait("@getTodo");
-    cy.get("@container").get(".todo-list").children().should("have.length", 0);
-    cy.get("@container").get(".todo-count strong").should("text", "0");
+    cy.get("@container").find('label').contains("update").should("not.be.empty");
   });
 
-  it("Team을 삭제한다.", () => {
-    cy.get("[data-ref=removeMember]").click();
+  it("todoItem을 더블 클릭시 input 모드로 변경", () => {
+    cy.get(".todoapp-container:eq(0) .todo-list li:eq(0)").as("container");
+    cy.get("@container").find("label").trigger('dblclick');
+    cy.get("@container").should("have.class", "editing");
+    cy.get("@container").find("input.edit").focus().type(`{esc}`, { force: true });
+    cy.get("@container").should("not.have.class", "editing");
+  });
+
+  it("todoItem을 삭제하기", () => {
+    cy.get(".todoapp-container:eq(0) .todo-list li:eq(0)").as("container");
+    cy.get("@container").find(".destroy").invoke('show').click();
+    cy.intercept("GET", `${baseURL}/teams/**/members/**`).as("getTodo");
+    cy.wait("@getTodo");
+    cy.get(".todoapp-container:eq(0) .todo-count strong").should("text", "0");
+  });
+
+  it("team을 삭제하기", () => {
+    cy.get("#user-title button").click();
   });
 });
