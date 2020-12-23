3e1dfe46a2f93e23f8817ed76b34308b0f1b78be
junilhwang <tjsdlf4261@naver.com>
2020-12-23 17:37:18 +0900

    test: todo item 수정 모드, 뷰 모드 테스트 완료

--- cypress/integration/todolist.spec.js
+++ cypress/integration/todolist.spec.js
@@ -36,21 +36,45 @@ describe("TodoList 테스트", () => {
     cy.get("@container").get(".todo-list li").eq(0).contains(newTodoText).should("be.visible");
   });
 
-  it("todoItem을 수정한다", () => {
+  it("todo list의 체크박스를 클릭하여 complete 상태로 변경.", () => {
+    // li tag 에 completed class 추가, input 태그에 checked 속성 추가
+    cy.intercept("PUT", `${baseURL}/teams/**/members/**/items/**/toggle`).as("toggleTodo");
+    cy.get(".todoapp-container").first().get(".todo-list li").as("container");
+    cy.get("@container").get(".toggle").first().click();
+    cy.wait("@toggleTodo");
+    cy.get("@container")
+      .should("have.class", "completed")
+        .get(".toggle")
+        .should("be.checked");
+
+    cy.get("@container").get(".toggle").first().click();
+    cy.wait("@toggleTodo");
+    cy.get("@container")
+      .should("not.have.class", "completed")
+        .get(".toggle")
+        .should("be.checked");
+  });
+
+  it("todoItem을 더블 클릭시 input 모드로 변경", () => {
+    cy.get(".todoapp-container").first().get(".todo-list li").as("container");
+    cy.get("@container").get("label").dblclick();
+    cy.get("@container").should("have.class", "editing");
+  });
+
+  it("todoItem의 수정 상태에서 esc 입력시 view 모드로 변경", () => {
+    cy.get(".todoapp-container").first().get(".todo-list li").as("container");
+    cy.get("@container").get("input").type(`{esc}`)
+    cy.get("@container").should("not.have.class", "editing");
+  });
+
+  it("todoItem을 수정", () => {
     const updatedTodoText = `${newTodoText} update`;
     cy.intercept("PUT", `${baseURL}/teams/**/members/**/items/**`).as("putTodo");
-    cy.get(".todoapp-container").first().as("container");
+    cy.get(".todoapp-container").first().get(".todo-list li").as("container");
     cy.get("@container").get(".todo-list li label").first().get('label').dblclick();
     cy.focused().type(" update").type('{enter}');
     cy.wait("@putTodo");
     cy.get("@container").get(".todo-list li label").first().get('label').should("text", updatedTodoText)
-  });
-
-  it("todo list의 체크박스를 클릭하여 complete 상태로 변경.", () => {
-    // li tag 에 completed class 추가, input 태그에 checked 속성 추가
-    cy.get(".todoapp-container").first().as("container");
-    cy.get("@container").get(".todo-list li .toggle").first().click();
-    cy.get("#todo-list li").eq(0).should("have.class", "completed")
   });
   //
   // it("todoItem을 삭제한다", () => {
